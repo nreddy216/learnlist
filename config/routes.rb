7f442ea9b50fdba9b4df0745ceee299eda314024
nreddy216 <nreddy216@gmail.com>
2016-02-25 08:07:30 -0800

    add sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  #USERS
 
   # STATIC PAGES
 
   root to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+
+  #USERS
+
+  get '/signup', to: 'users#new'
 
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,4 +18,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "Help | #{@base_title}"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
 end
