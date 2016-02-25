a5128abb0f688506eb8226e031f9fa7166472f26
nreddy216 <nreddy216@gmail.com>
2016-02-25 07:24:33 -0800

    finish static pages layout and static routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   #USERS
 
   # STATIC PAGES
-  get '/home', to: 'static_pages#home'
+
+  root to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
 
 end
