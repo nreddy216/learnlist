4c12948079289778eebe83179879bdfbd3b4fa14
nreddy216 <nreddy216@gmail.com>
2016-02-24 21:54:16 -0800

    added two controller tests for static pages and yielded title in layout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def about
+    render :about
+  end
+
+  def help
+    render :help
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "Learnlist | About"
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select "title", "Learnlist | Help"
   end
 
 end
